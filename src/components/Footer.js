dbc5db32c3f3e140ee7d654ba534cc00fcca4702
sebokdas1 <sebok.das.ff@gmail.com>
2023-02-26 11:14:57 +0600

    handle footer status

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { statusChanged } from "../redux/filters/actions";
 
 const incompleteTodoNumber = (todo_number) => {
   switch (todo_number) {
@@ -12,17 +13,39 @@ const incompleteTodoNumber = (todo_number) => {
   }
 };
 export default function Footer() {
+  const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  const { status, color } = filters;
   const todoRemaining = todos.filter((todo) => !todo.completed).length;
+
+  const statusChange = (status) => {
+    dispatch(statusChanged(status));
+  };
   return (
     <div className="mt-4 flex justify-between text-xs text-gray-500">
       <p>{incompleteTodoNumber(todoRemaining)} left</p>
       <ul className="flex space-x-1 items-center text-xs">
-        <li className="cursor-pointer font-bold">All</li>
+        <li
+          className={`cursor-pointer ${status === "All" && "font-bold"}`}
+          onClick={() => statusChange("All")}
+        >
+          All
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Incomplete</li>
+        <li
+          className={`cursor-pointer ${status === "Incomplete" && "font-bold"}`}
+          onClick={() => statusChange("Incomplete")}
+        >
+          Incomplete
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Complete</li>
+        <li
+          className={`cursor-pointer ${status === "Complete" && "font-bold"}`}
+          onClick={() => statusChange("Complete")}
+        >
+          Complete
+        </li>
         <li></li>
         <li></li>
         <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
