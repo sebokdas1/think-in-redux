d7e5f998d807a5a4b90bf22db794e5e8aa0644ad
sebokdas1 <sebok.das.ff@gmail.com>
2023-02-26 13:39:21 +0600

    handle todo add

--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -19,6 +19,8 @@ const reducer = (state = initialState, action) => {
         ...state,
         {
           id: nextTodoId(state),
+          text: action.payload,
+          completed: false,
         },
       ];
 
