b5e921da6368b8fea813ac24a8a926a48654573d
sebokdas1 <sebok.das.ff@gmail.com>
2023-02-26 12:05:43 +0600

    filter by status

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,24 @@ import Todo from "./Todo";
 
 export default function TodoList() {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {todos.map((todo) => (
-        <Todo todo={todo} key={todo.id} />
-      ))}
+      {todos
+        .filter((todo) => {
+          const { status } = filters;
+          switch (status) {
+            case "Complete":
+              return todo.completed;
+            case "Incomplete":
+              return !todo.completed;
+            default:
+              return true;
+          }
+        })
+        .map((todo) => (
+          <Todo todo={todo} key={todo.id} />
+        ))}
     </div>
   );
 }
